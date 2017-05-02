397a961a2118bf1b674b5c81778c7ff991bfed2f
HugoMoy <hugo.moynac@gmail.com>
2017-05-02 10:30:18 +0200

    last ver

---
+++ PACHINKO/Assets/WinOnContact.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinOnContact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        System.Random random = new System.Random();
+        int randomNumber = random.Next(-1, 3);
+        Vector3 pos = new Vector3(randomNumber, 6, -2);
+        Destroy(other.gameObject);
+
+        Instantiate((Resources.Load("Sphere")) as GameObject, pos, Quaternion.identity);
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+       
+
+    }
+}
