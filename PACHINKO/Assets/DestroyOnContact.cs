6a946f8726c607c6e6eada757c0641cb734cfb93
HugoMoy <hugo.moynac@gmail.com>
2017-05-02 09:37:28 +0200

    New ver

---
+++ PACHINKO/Assets/DestroyOnContact.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DestroyOnContact : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        System.Random random = new System.Random();
+        int randomNumber = random.Next(-1, 3);
+        Vector3 pos = new Vector3(randomNumber, 6, -2);/*
+        other.gameObject.transform.position.x = randomNumber;
+        other.gameObject.transform.position.y = 6;
+        other.gameObject.transform.position.z = -2;*/
+        Destroy(other.gameObject);
+        //GameObject.ge
+        //GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //other.transform.position = new Vector3(randomNumber, 6, -2);
+        //other.transform.
+
+        Instantiate((Resources.Load("Sphere")) as GameObject, pos, Quaternion.identity);
+        //other.gameObject.transform.Translate(Vector3.left * 10);
+       
+    }
+    
+}
---
+++ PACHINKO/Assets/KinectData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+static class KinectData
+{
+        public static float mainDX { get; set; }
+        public static float mainDY { get; set; }
+        public static float mainDZ { get; set; }
+        public static float mainGX { get; set; }
+        public static float mainGY { get; set; }
+        public static float mainGZ { get; set; }
+        public static float teteX { get; set; }
+        public static float teteY { get; set; }
+        public static float teteZ { get; set; }
+}
+
