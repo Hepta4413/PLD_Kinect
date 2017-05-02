6bed311797737f26578c80dbfdbf3c2ee4d668d0
HugoMoy <hugo.moynac@gmail.com>
2017-05-02 08:32:44 +0200

    Add pachinko project

---
+++ PACHINKO/Assets/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    int i = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(i%500 < 250)
+            transform.Translate(Vector3.left * Time.deltaTime, Space.World);
+        else
+            transform.Translate(Vector3.right * Time.deltaTime, Space.World);
+
+        i++;
+    }
+}
