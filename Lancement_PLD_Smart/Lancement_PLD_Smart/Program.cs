6ade2bb4c3a7b81966b18cca66ccf48d3d4defdf
lpeter <ludovicpeter1@gmail.com>
2017-05-03 09:29:46 +0200

    Lancement_PLD_Smart.sln

---
+++ Lancement_PLD_Smart/Lancement_PLD_Smart/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lancement_PLD_Smart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start(Environment.CurrentDirectory + @"\kinect");
+            System.Threading.Thread.Sleep(2000);
+            System.Diagnostics.Process.Start(Environment.CurrentDirectory + @"\BreakOutGame");
+        }
+    }
+}
