322e2dbec64bfda5904f63d3f21f1a231ee26994
Anthony Delay <anthony.delay@outlook.fr>
2017-05-03 09:51:31 +0200

    Update pachinko with hazards

---
+++ PACHINKO/Assets/MovingHazards.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingHazards : MonoBehaviour {
+
+    public float speed = 1.5f;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position += Vector3.right * speed * Time.deltaTime;
+
+        if (transform.position.x < 1)
+        {
+            speed = -speed;
+        }
+        else if(transform.position.x > 2)
+        {
+            speed = -speed;
+        }
+    }
+}
---
+++ PACHINKO/Assets/RotateHazard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateHazard : MonoBehaviour {
+
+    private Vector3 rotation;
+    public int angle;
+
+	// Use this for initialization
+	void Start () {
+        rotation = new Vector3(0, 0, angle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotation);
+    }
+}
